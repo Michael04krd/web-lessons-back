538ecd16ca66b050a8ac56084bfd99a2472c6453
Michael04krd <bob.michael0405@mail.ru>
2025-05-05 12:20:40 +0300

    Update admin.php

--- 6/admin.php
+++ 6/admin.php
@@ -162,6 +162,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['save_edit'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Административная панель</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="admin.css">
 </head>
 <body>
     <div class="admin-container">
