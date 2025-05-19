daf278b73f2df404e1ef99951687b7403d9a8b1f
Michael04krd <bob.michael0405@mail.ru>
2025-05-19 20:48:28 +0300

    Update logout.php

--- 7/logout.php
+++ 7/logout.php
@@ -1,16 +1,8 @@
 <?php
 session_start();
 
-// Уничтожаем сессию
-$_SESSION = array();
-if (ini_get("session.use_cookies")) {
-    $params = session_get_cookie_params();
-    setcookie(session_name(), '', time() - 42000,
-        $params["path"], $params["domain"],
-        $params["secure"], $params["httponly"]
-    );
-}
-session_destroy();
+// Уничтожаем сессию админки
+unset($_SESSION['admin_logged_in']);
 
 // Сбрасываем HTTP Basic Auth
 header('HTTP/1.1 401 Unauthorized');
