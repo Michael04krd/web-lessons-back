f7e9a16c396b15a909e3f9b7c7572bffd50441d8
Michael04krd <bob.michael0405@mail.ru>
2025-05-19 21:23:44 +0300

    Update create_admin.php

--- 7/create_admin.php
+++ 7/create_admin.php
@@ -1,11 +1,21 @@
 <?php
-$db = require 'db.php';
+$db = new PDO('mysql:host=localhost;dbname=u68608', 'u68608', '1096993', [
+    PDO::ATTR_PERSISTENT => true,
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+]);
 
 $login = 'admin';
-$password = 'admin123'; // Ваш пароль
+$password = 'admin123';
 
-$stmt = $db->prepare("INSERT INTO admins (login, password_hash) VALUES (?, ?)");
-$stmt->execute([$login, password_hash($password, PASSWORD_DEFAULT)]);
-
-echo "Админ создан. Логин: $login, Пароль: $password";
+try {
+    $stmt = $db->prepare("INSERT INTO admins (login, password_hash) VALUES (?, ?)");
+    $stmt->execute([$login, password_hash($password, PASSWORD_DEFAULT)]);
+    
+    echo "Администратор создан:<br>";
+    echo "Логин: $login<br>";
+    echo "Пароль: $password<br>";
+    echo '<a href="admin.php">Перейти в админку</a>';
+} catch (PDOException $e) {
+    die("Ошибка создания администратора");
+}
 ?>
