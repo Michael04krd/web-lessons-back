04ddf458f49a712971e6f61d8a445ec026d90486
Michael04krd <bob.michael0405@mail.ru>
2025-06-02 01:52:41 +0300

    Update admin.tpl.php

--- project/theme/admin.tpl.php
+++ project/theme/admin.tpl.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>Панель администрирования</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/web-lessons-back.github.io/project/styles/admin.css">
+    <link rel="stylesheet" href="/web-lessons-back/project/styles/admin.css">
 </head>
 <body>
     <div class="admin-container">
